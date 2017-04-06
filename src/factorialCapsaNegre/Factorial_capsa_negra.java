e2640afcf62b2db9558b98b635822cc766142a75
jaime <jaime@jaime-Aspire-5742>
2017-04-06 21:01:10 +0200

    Añadir metodo suma

--- src/factorialCapsaNegre/Factorial_capsa_negra.java
+++ src/factorialCapsaNegre/Factorial_capsa_negra.java
@@ -9,6 +9,10 @@ public class Factorial_capsa_negra {
         return n * factorial(n - 1);
     }
 
+    private static int suma(int a,int b){
+    	return a + b;
+    }
+
     public static void main(String[] args){
         System.out.println(factorial(3));
     }
