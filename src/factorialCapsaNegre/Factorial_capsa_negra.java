c76385ae1db6395ab17dd2755b8f99c7300777dc
alex <alex@alex>
2017-04-18 19:16:08 +0200

    subo la modificacion

--- src/factorialCapsaNegre/Factorial_capsa_negra.java
+++ src/factorialCapsaNegre/Factorial_capsa_negra.java
@@ -2,14 +2,18 @@ package factorialCapsaNegre;
 
 public class Factorial_capsa_negra {
 
-    private static int factorial(int n){
-        if(n == 0 || n == 1){
-            return 1;
-        }
-        return n * factorial(n - 1);
-    }
+	private static int factorial(int n){
+		int res=0;
+		if(n == 0 || n == 1){
+			return 1;
+		}
+		for(int i=n; i>=n; i--) //Se realiza la iteracion mientras i sea menor o igual a n se entrara al ciclo y se realizara la operacion ya despues se imprime el resultado del factorial
+			res=res*i;
 
-    public static void main(String[] args){
-        System.out.println(factorial(3));
-    }
+		return n * factorial(n - 1);
+	}
+
+	public static void main(String[] args){
+		System.out.println(factorial(3));
+	}
 }
